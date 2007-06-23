0ee1af4449c3e3fe2533f59866f2a46fdac9ba30
Ivan Heffner <iheffner@gmail.com>
2007-06-23 10:01:18 +0000

    added calls to MyPodcast to build RSS and add ID3v2 tags

--- mk_podcast.pl
+++ mk_podcast.pl
@@ -4,11 +4,7 @@ use strict;
 use warnings;
 
 use Getopt::Long;
-
-use MP3::ID3v1Tag;
-#use XML::RSS;
-#use MP3::Tag;
-#see source for MP3::Podcast for some usage pointers
+use Time::HiRes qw( time sleep );
 
 use MyPodcasts;
 
@@ -24,7 +20,7 @@ if ( $list ) {
 	exit(0);
 }
 
-warn "looking up $podcast\n";
+# look up podcast in MyPodcasts
 my %podcast = ($podcast) ? MyPodcasts->getConfig( $podcast ) : ();
 
 die "$podcast: invalid podcast\n" unless $podcast{'source'};
@@ -32,30 +28,29 @@ die "$podcast: invalid podcast\n" unless $podcast{'source'};
 
 if ( my $pid = fork() ) {
 	# parent
-	sleep( $podcast{'duration'} + 5 ); # Add promo time
+	my $start_time = time;
+	sleep( $podcast{'duration'} - (time() - $start_time) + 5 ); # Add promo time
 	kill 'INT', $pid;
 	
 	# use MP3::ID3v1Tag to set Name, source, description, title, artist, etc.
-	my $mp3_file = new MP3::ID3v1Tag($podcast{dumpfile});
-	$mp3_file->set_title($podcast{title});
-	$mp3_file->set_artist($podcast{artist});
-	$mp3_file->set_album($podcast{name});
-	$mp3_file->set_comment($podcast{source});
-	$mp3_file->set_genre('Podcast');
-	$mp3_file->save();
+	MyPodcasts->add_ID3_tag( $podcast );
 
-	# TBD: need to post dump file to appropriate blog
-	# TBD: remove dumpfile
-	# XXX: This is temporary!
-	rename $podcast{dumpfile}, "podcasts/$podcast{filename}";
+	# move dump file to appropriate destination
+	mkdir qq|$ENV{HOME}/podcasts/$podcast| unless ( -d qq|$ENV{HOME}/podcasts/$podcast| );
+	link $podcast{'dumpfile'}, qq|$ENV{HOME}/podcasts/$podcast/$podcast{'filename'}|;
+
+	# update the RSS file
+	MyPodcasts->buildRSS($podcast);
+
+	# remove the dump file
+	unlink $podcast{'dumpfile'};
 }
 elsif ( defined $pid ) {
 	# child
-	my @cmd = ( qw( /usr/bin/mplayer -msglevel all=-1 -dumpstream -dumpfile ),
+	my @cmd = ( qw( /usr/bin/mplayer -really-quiet -msglevel all=-1 -dumpstream -dumpfile ),
 		$podcast{'dumpfile'},
 		$podcast{'source'},
 	);
-	warn join(' ',@cmd);
 	exec( @cmd ) || die "failed to capture stream: $!\n";
 }
 else {
