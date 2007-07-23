e316c89d5f0c750c498d1c5c417e2eea4b0b25dd
Ivan Heffner <iheffner@gmail.com>
2007-07-23 20:04:13 +0000

    added ability to capture and build feed separately; needs work

--- mk_podcast.pl
+++ mk_podcast.pl
@@ -8,10 +8,12 @@ use Time::HiRes qw( time sleep );
 
 use MyPodcasts;
 
-my ($podcast, $list);
+my ($podcast, $capture, $feed, $list) = (undef, 1, 1, undef);
 
 GetOptions(
 	"podcast=s" => \$podcast,
+	"capture!"  => \$capture,
+	"feed!"     => \$feed,
 	"list"      => \$list,
 );
 
@@ -25,15 +27,38 @@ my %podcast = ($podcast) ? MyPodcasts->getConfig( $podcast ) : ();
 
 die "$podcast: invalid podcast\n" unless $podcast{'source'};
 
+capture_stream($podcast) if ($capture);
+build_feed(    $podcast) if ($feed);
 
-if ( my $pid = fork() ) {
-	# parent
-	my $start_time = time;
-	sleep( $podcast{'duration'} - (time() - $start_time) );
-	kill 'INT', $pid;
-	
-	# use MP3::ID3v1Tag to set Name, source, description, title, artist, etc.
-	MyPodcasts->add_ID3_tag( $podcast );
+exit(0);
+
+sub capture_stream {
+	if ( my $pid = fork() ) {
+		# parent
+		my $start_time = time;
+		sleep( $podcast{'duration'} - (time() - $start_time) );
+		kill 'INT', $pid;
+
+		# use MP3::ID3v1Tag to set Name, source, description, title, artist, etc.
+		MyPodcasts->add_ID3_tag( $podcast );
+	}
+	elsif ( defined $pid ) {
+		# child
+		my @cmd = ( qw( /usr/bin/mplayer -really-quiet -msglevel all=-1 -dumpstream -dumpfile ),
+			$podcast{'dumpfile'},
+			$podcast{'source'},
+		);
+		exec( @cmd ) || die "failed to capture stream: $!\n";
+	}
+	else {
+		# error
+		die "Failed to fork(): $!\n";
+	}
+}
+
+sub build_feed {
+	die "cannot build feed for $podcast{'name'}: $podcast{'dumpfile'} does not exist\n"
+			unless (-e $podcast{'dumpfile'});
 
 	# move dump file to appropriate destination
 	mkdir qq|$ENV{HOME}/podcasts/$podcast| unless ( -d qq|$ENV{HOME}/podcasts/$podcast| );
@@ -45,17 +70,4 @@ if ( my $pid = fork() ) {
 	# remove the dump file
 	unlink $podcast{'dumpfile'};
 }
-elsif ( defined $pid ) {
-	# child
-	my @cmd = ( qw( /usr/bin/mplayer -really-quiet -msglevel all=-1 -dumpstream -dumpfile ),
-		$podcast{'dumpfile'},
-		$podcast{'source'},
-	);
-	exec( @cmd ) || die "failed to capture stream: $!\n";
-}
-else {
-	# error
-	die "Failed to fork(): $!\n";
-}
 
-exit(0);
