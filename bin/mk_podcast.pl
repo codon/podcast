9082e15fbaf7f36e7e488b891e58ff1e18d05c21
Ivan Heffner <iheffner@gmail.com>
2011-03-06 04:44:49 -0800

    retabbing w/expandtab on

--- bin/mk_podcast.pl
+++ bin/mk_podcast.pl
@@ -7,10 +7,10 @@ use File::Path qw(mkpath);
 use Getopt::Long;
 use Time::HiRes qw( time sleep );
 use WWW::Curl::Easy qw(
-	CURLOPT_BUFFERSIZE
-	CURLOPT_TIMEOUT
-	CURLOPT_WRITEDATA
-	CURLOPT_URL
+    CURLOPT_BUFFERSIZE
+    CURLOPT_TIMEOUT
+    CURLOPT_WRITEDATA
+    CURLOPT_URL
 );
 
 use MyPodcasts;
@@ -19,35 +19,35 @@ my ($podcast, $capture, $feed, $list, $daysago, $basedir, $baseurl, $help, $over
    ( undef,    1,        1,    undef,  0,        undef,    undef,    0,     0);
 
 GetOptions(
-	'podcast=s' => \$podcast,
-	'capture!'  => \$capture,
-	'feed!'     => \$feed,
-	'list'      => \$list,
-	'basedir=s' => \$basedir,
-	'baseurl=s' => \$baseurl,
-	'daysago=i' => \$daysago,
-	'help'      => \$help,
+    'podcast=s' => \$podcast,
+    'capture!'  => \$capture,
+    'feed!'     => \$feed,
+    'list'      => \$list,
+    'basedir=s' => \$basedir,
+    'baseurl=s' => \$baseurl,
+    'daysago=i' => \$daysago,
+    'help'      => \$help,
     overwrite   => \$overwrite,
 );
 
 my $Podcast = MyPodcasts->new(
-	podcast => $podcast,
-	basedir => $basedir,
-	baseurl => $baseurl,
-	daysago => $daysago,
+    podcast => $podcast,
+    basedir => $basedir,
+    baseurl => $baseurl,
+    daysago => $daysago,
 );
 
 if ( $list ) {
-	print "Known Podcasts: \n\t".join("\n\t", $Podcast->list())."\n";
-	exit(0);
+    print "Known Podcasts: \n\t".join("\n\t", $Podcast->list())."\n";
+    exit(0);
 }
 
 # look up podcast in $Podcast
 my %podcast = ($podcast) ? $Podcast->get_Config() : ();
 
 if ( $help || !$podcast{'source'} ) {
-	warn "$podcast: invalid podcast\n" if ($podcast && !$podcast{'source'});
-	die usage();
+    warn "$podcast: invalid podcast\n" if ($podcast && !$podcast{'source'});
+    die usage();
 }
 
 capture_stream($Podcast) if ($capture);
@@ -56,55 +56,55 @@ build_feed(    $Podcast) if ($feed);
 exit(0);
 
 sub capture_stream {
-	my $podcast = shift;
+    my $podcast = shift;
 
     if ( -e $podcast->{'destfile'} && ! $overwrite ) {
         die "$podcast->{'destfile'} already exists!";
     }
-	# open the destination file
-	open my $mp3, '>', $podcast->{'destfile'} or die "could not open destination file: $!\n";
+    # open the destination file
+    open my $mp3, '>', $podcast->{'destfile'} or die "could not open destination file: $!\n";
 
-	# instatiate and set up the Curl client
-	my $curl = WWW::Curl::Easy->new();
-	$curl->setopt(CURLOPT_BUFFERSIZE,0);                     # do not buffer; write straight to disk
-	$curl->setopt(CURLOPT_TIMEOUT,$podcast->{'duration'});   # how long to capture the stream (in seconds)
-	$curl->setopt(CURLOPT_WRITEDATA,$mp3);                   # where to write the data
-	$curl->setopt(CURLOPT_URL,$podcast->{'source'});         # the url to capture
+    # instatiate and set up the Curl client
+    my $curl = WWW::Curl::Easy->new();
+    $curl->setopt(CURLOPT_BUFFERSIZE,0);                     # do not buffer; write straight to disk
+    $curl->setopt(CURLOPT_TIMEOUT,$podcast->{'duration'});   # how long to capture the stream (in seconds)
+    $curl->setopt(CURLOPT_WRITEDATA,$mp3);                   # where to write the data
+    $curl->setopt(CURLOPT_URL,$podcast->{'source'});         # the url to capture
 
-	# make the call
-	my $rc = $curl->perform();
+    # make the call
+    my $rc = $curl->perform();
 
-	if ( 28 == $rc ) { # expect a timeout; this is a continuous stream we're grabbing...
-		# ok
-		$podcast->add_ID3_tag( $daysago );
-	}
-	else {
-		# problems XXX TBD Better error handling?
-		$curl->strerror($rc);
-	}
+    if ( 28 == $rc ) { # expect a timeout; this is a continuous stream we're grabbing...
+        # ok
+        $podcast->add_ID3_tag( $daysago );
+    }
+    else {
+        # problems XXX TBD Better error handling?
+        $curl->strerror($rc);
+    }
 
-	return;
+    return;
 }
 
 sub build_feed {
-	my $podcast = shift;
+    my $podcast = shift;
 
-	die "cannot build feed for $podcast{'name'}: $podcast{'destfile'} does not exist\n"
-			unless (-e $podcast{'destfile'});
+    die "cannot build feed for $podcast{'name'}: $podcast{'destfile'} does not exist\n"
+            unless (-e $podcast{'destfile'});
 
-	# update the RSS file
-	$Podcast->build_RSS($podcast, $daysago);
+    # update the RSS file
+    $Podcast->build_RSS($podcast, $daysago);
 }
 
 sub usage {
-	return qq{
-		--podcast=<string>                          Specify a podcast
-		--nocapture                                 Do not capture the stream
-		--nofeed                                    Do not build the RSS feed
-		--list                                      List all known podcasts
-		--basedir=<path/to/save/dir>                Base directory for saving streams
-		--baseurl=<http://example.com/examples>     Base url for RSS feed
-		--daysago=n                                 Look for media file N days ago
-		--help                                      Print this usage
-	\n};
+    return qq{
+        --podcast=<string>                          Specify a podcast
+        --nocapture                                 Do not capture the stream
+        --nofeed                                    Do not build the RSS feed
+        --list                                      List all known podcasts
+        --basedir=<path/to/save/dir>                Base directory for saving streams
+        --baseurl=<http://example.com/examples>     Base url for RSS feed
+        --daysago=n                                 Look for media file N days ago
+        --help                                      Print this usage
+    \n};
 }
--- lib/MyPodcasts.pm
+++ lib/MyPodcasts.pm
@@ -16,215 +16,215 @@ use constant 'HRS'  => 60 * 60     ; # seconds in an hour
 use constant 'DAYS' => 60 * 60 * 24; # seconds in a day
 
 my %month = ( 0  => undef,
-	1  => 'January', 2  => 'February', 3  => 'March', 4  => 'April', 5  => 'May', 6  => 'June',
-	7  => 'July', 8  => 'August', 9  => 'September', 10 => 'October', 11 => 'November', 12 => 'December',
+    1  => 'January', 2  => 'February', 3  => 'March', 4  => 'April', 5  => 'May', 6  => 'June',
+    7  => 'July', 8  => 'August', 9  => 'September', 10 => 'October', 11 => 'November', 12 => 'December',
 );
 my %day = ( 0 => 'Sun', 1 => 'Mon', 2 => 'Tue', 3 => 'Wed', 4 => 'Thu', 5 => 'Fri', 6 => 'Sat' );
 
 sub new {
-	my($class, @args) = @_;
-	my $self = bless {@args}, $class;
-	$self->{'config'} = $self->_find_confs();
-	$self->{'daysago'} ||= 0; # default to today (now);
-	$self->{'basedir'} ||= "$ENV{HOME}/podcasts";
-	$self->{'baseurl'} ||= 'http://example.com/podcasts';
+    my($class, @args) = @_;
+    my $self = bless {@args}, $class;
+    $self->{'config'} = $self->_find_confs();
+    $self->{'daysago'} ||= 0; # default to today (now);
+    $self->{'basedir'} ||= "$ENV{HOME}/podcasts";
+    $self->{'baseurl'} ||= 'http://example.com/podcasts';
 
-	if ( defined $self->{'podcast'} and exists $self->{'config'}{ $self->{'podcast'} } ) {
-		my %config = $self->get_Config();
-		@$self{keys %config} = values %config;
-	}
-	return $self;
+    if ( defined $self->{'podcast'} and exists $self->{'config'}{ $self->{'podcast'} } ) {
+        my %config = $self->get_Config();
+        @$self{keys %config} = values %config;
+    }
+    return $self;
 }
 
 sub _parse_conf {
-	my ( $self, $file ) = @_;
+    my ( $self, $file ) = @_;
 
-	my $config = eval {
-		my $tmp_config = do "$file";
+    my $config = eval {
+        my $tmp_config = do "$file";
 
-		if ( defined $tmp_config and not ( ref($tmp_config) eq 'HASH' ) ) {
-			warn "$file: invalid podcast file\n";
-			return;
-		}
+        if ( defined $tmp_config and not ( ref($tmp_config) eq 'HASH' ) ) {
+            warn "$file: invalid podcast file\n";
+            return;
+        }
 
-		return $tmp_config;
-	};
-	if ($@) {
-		warn "WARNING: $@\n";
-		$config = {};
-	}
+        return $tmp_config;
+    };
+    if ($@) {
+        warn "WARNING: $@\n";
+        $config = {};
+    }
 
-	return $config;
+    return $config;
 }
 
 sub _find_confs {
-	my ( $self ) = @_;
+    my ( $self ) = @_;
 
-	# currently confs are in a MyPodcasts dir beside this file
-	( my $conf_dir = __FILE__ ) =~ s/\.pm$//;
+    # currently confs are in a MyPodcasts dir beside this file
+    ( my $conf_dir = __FILE__ ) =~ s/\.pm$//;
 
-	my %kampf;
-	if ( -d $conf_dir && -r $conf_dir ) {
-		opendir my $dir, $conf_dir or die "cannot open $conf_dir: $!\n";
-		%kampf =
-			map  { @$_ }                      # expand the transform into a hash
-			grep { -f $_->[1] }               # make sure that it is a file
-			map  { [ $_ => "$conf_dir/$_" ] } # use the Shwartz(-ian transform)
-			grep { /^\w/ }                    # require conf filenames to start with a word character
-			readdir $dir;                     # get everything in the confdir
-	}
+    my %kampf;
+    if ( -d $conf_dir && -r $conf_dir ) {
+        opendir my $dir, $conf_dir or die "cannot open $conf_dir: $!\n";
+        %kampf =
+            map  { @$_ }                      # expand the transform into a hash
+            grep { -f $_->[1] }               # make sure that it is a file
+            map  { [ $_ => "$conf_dir/$_" ] } # use the Shwartz(-ian transform)
+            grep { /^\w/ }                    # require conf filenames to start with a word character
+            readdir $dir;                     # get everything in the confdir
+    }
 
-	return \%kampf
+    return \%kampf
 }
 
 sub get_Config {
-	my ($self) = (@_);
+    my ($self) = (@_);
 
-	# if we didn't get a podcast name, we can't really come up with its config
-	my $podcast = $self->{'podcast'} || return;
+    # if we didn't get a podcast name, we can't really come up with its config
+    my $podcast = $self->{'podcast'} || return;
 
-	# skip this work if we've already parsed this config
-	return ( %{ $self->{'config'}{$podcast} } ) if ( ref( $self->{'config'}{$podcast} ) );
+    # skip this work if we've already parsed this config
+    return ( %{ $self->{'config'}{$podcast} } ) if ( ref( $self->{'config'}{$podcast} ) );
 
-	my $config = $self->_parse_conf( $self->{'config'}{$podcast} );
+    my $config = $self->_parse_conf( $self->{'config'}{$podcast} );
 
-	my ($mday,$mon,$year) = (localtime(time() - ($self->{'daysago'} * DAYS)))[3 .. 5];
-	$mon++; $year+=1900;
+    my ($mday,$mon,$year) = (localtime(time() - ($self->{'daysago'} * DAYS)))[3 .. 5];
+    $mon++; $year+=1900;
 
-	$config->{'title'}    = sprintf('%s for %s %02d, %4d', $config->{'name'},$month{$mon},$mday,$year);
-	$config->{'filename'} = sprintf('%s-%4d-%02d-%02d.mp3',$config->{'name'},$year, $mon, $mday);
-	$config->{'filename'} =~ s/\s+/_/g;
-	$config->{'rss_file'} = "$self->{'basedir'}/$podcast.xml";
-	$config->{'destfile'} = "$self->{'basedir'}/$podcast/$config->{'filename'}";
+    $config->{'title'}    = sprintf('%s for %s %02d, %4d', $config->{'name'},$month{$mon},$mday,$year);
+    $config->{'filename'} = sprintf('%s-%4d-%02d-%02d.mp3',$config->{'name'},$year, $mon, $mday);
+    $config->{'filename'} =~ s/\s+/_/g;
+    $config->{'rss_file'} = "$self->{'basedir'}/$podcast.xml";
+    $config->{'destfile'} = "$self->{'basedir'}/$podcast/$config->{'filename'}";
 
-	# Ensure the destination dir exists
-	mkpath( "$self->{'basedir'}/$podcast" ) unless ( -d "$self->{'basedir'}/$podcast" );
+    # Ensure the destination dir exists
+    mkpath( "$self->{'basedir'}/$podcast" ) unless ( -d "$self->{'basedir'}/$podcast" );
 
-	$self->{'config'}{$podcast} = $config;
-	return %$config;
+    $self->{'config'}{$podcast} = $config;
+    return %$config;
 }
 
 sub list {
-	return keys %{ $_[0]->{'config'} };
+    return keys %{ $_[0]->{'config'} };
 }
 
 sub build_RSS {
-	my ($self) = @_;
+    my ($self) = @_;
 
-	my %config = $self->get_Config();
+    my %config = $self->get_Config();
 
-	my $rss = XML::RSS->new( version => '2.0' );
-	$rss->add_module(
-		prefix   => 'itunes',
-		uri      => 'http://www.itunes.com/dtds/podcast-1.0.dtd',
-		version  => '2.0',
-	);
+    my $rss = XML::RSS->new( version => '2.0' );
+    $rss->add_module(
+        prefix   => 'itunes',
+        uri      => 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+        version  => '2.0',
+    );
 
-	if ( -e $config{'rss_file'} and -s $config{'rss_file'} > 0 ) {
-		copy( $config{'rss_file'}, "$config{'rss_file'}.bak" );
-		$rss->parsefile($config{'rss_file'});
-		if (@{$rss->{'items'}} == 5) {
-			my $last_item = pop(@{$rss->{'items'}});
-			my $url = $last_item->{'enclosure'}->{'url'} || '';
-			$url =~ s[^$self->{'baseurl'}][$self->{'basedir'}/];
-			unlink $url if ( -e $url );
-		}
-	}
-	else {
-		$rss->channel(
-			title          => $config{'name'},
-			link           => $config{'home_page'},
-			language       => 'en-us',
-			itunes         => { map { $_ => $config{$_} } qw( subtitle summary author ) },
-			description    => $config{'description'},
-			copyright      => $config{'copyright'},
-		);
-	}
+    if ( -e $config{'rss_file'} and -s $config{'rss_file'} > 0 ) {
+        copy( $config{'rss_file'}, "$config{'rss_file'}.bak" );
+        $rss->parsefile($config{'rss_file'});
+        if (@{$rss->{'items'}} == 5) {
+            my $last_item = pop(@{$rss->{'items'}});
+            my $url = $last_item->{'enclosure'}->{'url'} || '';
+            $url =~ s[^$self->{'baseurl'}][$self->{'basedir'}/];
+            unlink $url if ( -e $url );
+        }
+    }
+    else {
+        $rss->channel(
+            title          => $config{'name'},
+            link           => $config{'home_page'},
+            language       => 'en-us',
+            itunes         => { map { $_ => $config{$_} } qw( subtitle summary author ) },
+            description    => $config{'description'},
+            copyright      => $config{'copyright'},
+        );
+    }
 
-	my $size = (stat $config{'destfile'})[7];
-	my %extraction = $config{'extract'}->( $self, $config{'home_page'} ); # fake an OO call
-	$extraction{'duration'} = _estimate_duration( $size );
-	my $description = delete $extraction{'summary'};
-	$rss->add_item(
-		title       => $extraction{'title'} || $extraction{'subtitle'},
-		itunes      => { %extraction },
-		description => $description,
-		category    => 'podcasts',
-		enclosure   => {
-			'url'    => sprintf('%s/%s/%s',$self->{'baseurl'},$self->{'podcast'},$config{'filename'}),
-			'type'   => "audio/mpeg",
-			'length' => $size,
-		},
-		mode        => 'insert',
-	);
+    my $size = (stat $config{'destfile'})[7];
+    my %extraction = $config{'extract'}->( $self, $config{'home_page'} ); # fake an OO call
+    $extraction{'duration'} = _estimate_duration( $size );
+    my $description = delete $extraction{'summary'};
+    $rss->add_item(
+        title       => $extraction{'title'} || $extraction{'subtitle'},
+        itunes      => { %extraction },
+        description => $description,
+        category    => 'podcasts',
+        enclosure   => {
+            'url'    => sprintf('%s/%s/%s',$self->{'baseurl'},$self->{'podcast'},$config{'filename'}),
+            'type'   => "audio/mpeg",
+            'length' => $size,
+        },
+        mode        => 'insert',
+    );
 
-	if (scalar @{ $rss->{'items'} || [] } > 1) {
-		my ($thing1,$thing2) = map {
-			$_->{'itunes'}{'summary'} || $_->{'description'} || ''
-		} @{$rss->{'items'}||[]}[0,1];
-		if ( length($thing1) > 0 and $thing1 eq $thing2) {
-			die "$config{'home_page'} has not been updated\n";
-		}
-	}
+    if (scalar @{ $rss->{'items'} || [] } > 1) {
+        my ($thing1,$thing2) = map {
+            $_->{'itunes'}{'summary'} || $_->{'description'} || ''
+        } @{$rss->{'items'}||[]}[0,1];
+        if ( length($thing1) > 0 and $thing1 eq $thing2) {
+            die "$config{'home_page'} has not been updated\n";
+        }
+    }
 
-	eval {
-		$rss->save($config{'rss_file'});
-	};
+    eval {
+        $rss->save($config{'rss_file'});
+    };
 
-	if ($@) {
-		warn $@;
-		if ( -e "$config{'rss_file'}.bak" ) {
-			rename( "$config{'rss_file'}.bak", $config{'rss_file'} );
-		}
-	}
-	else {
-		if ( -e "$config{'rss_file'}.bak" ) {
-			unlink( "$config{'rss_file'}.bak" );
-		}
-	}
+    if ($@) {
+        warn $@;
+        if ( -e "$config{'rss_file'}.bak" ) {
+            rename( "$config{'rss_file'}.bak", $config{'rss_file'} );
+        }
+    }
+    else {
+        if ( -e "$config{'rss_file'}.bak" ) {
+            unlink( "$config{'rss_file'}.bak" );
+        }
+    }
 
-	return;
+    return;
 }
 
 sub add_ID3_tag {
-	my ($self) = @_;
+    my ($self) = @_;
 
-	my %config = $self->get_Config();
+    my %config = $self->get_Config();
 
-	my $mp3_file = MP3::Tag->new($config{'destfile'}) || die "could not instatiate MP3::Tag: $!";
-	my $id3v2 = $mp3_file->new_tag('ID3v2');
-	$id3v2->add_frame('TIT1','Podcast');
-	$id3v2->add_frame('TIT2',$config{'title'});
-	$id3v2->add_frame('TOPE',$config{'artist'});
-	$id3v2->write_tag();
+    my $mp3_file = MP3::Tag->new($config{'destfile'}) || die "could not instatiate MP3::Tag: $!";
+    my $id3v2 = $mp3_file->new_tag('ID3v2');
+    $id3v2->add_frame('TIT1','Podcast');
+    $id3v2->add_frame('TIT2',$config{'title'});
+    $id3v2->add_frame('TOPE',$config{'artist'});
+    $id3v2->write_tag();
 
-	return;
+    return;
 }
 
 sub add_Lyrics {
-	my ($file,$lyrics) = @_;
+    my ($file,$lyrics) = @_;
 
-	my $mp3_file = MP3::Tag->new($file) || die "could not instatiate MP3::Tag: $!";
-	my $id3v2 = $mp3_file->new_tag('ID3v2');
-	$id3v2->add_frame('USLT','ASCII','en-US',$lyrics,$lyrics);
-	$id3v2->write_tag();
+    my $mp3_file = MP3::Tag->new($file) || die "could not instatiate MP3::Tag: $!";
+    my $id3v2 = $mp3_file->new_tag('ID3v2');
+    $id3v2->add_frame('USLT','ASCII','en-US',$lyrics,$lyrics);
+    $id3v2->write_tag();
 
-	return;
+    return;
 }
 
 sub get_pubDate {
-	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
-	# pubDate should be of the format: Sat, 9 Jun 2007 17:00:00 -0700
-	return sprintf('%3s, %d %s %4d %02d:%02d:%02d -0700',
-		$day{$wday}, $mday, $month{++$mon}, ($year + 1900), $hour, $min, $sec );
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
+    # pubDate should be of the format: Sat, 9 Jun 2007 17:00:00 -0700
+    return sprintf('%3s, %d %s %4d %02d:%02d:%02d -0700',
+        $day{$wday}, $mday, $month{++$mon}, ($year + 1900), $hour, $min, $sec );
 }
 
 sub _estimate_duration {
-	my $size = shift;
-	my $duration = ($size * 8) / (128 * 1024);
-	(my $hours, $duration) = ( ($duration / ( HRS )), ($duration % ( HRS )) );
-	(my $minutes, $duration) = ( ($duration / ( MIN )), ($duration % ( MIN )) );
-	return sprintf( '%02d:%02d:%02d', $hours, $minutes, $duration );
+    my $size = shift;
+    my $duration = ($size * 8) / (128 * 1024);
+    (my $hours, $duration) = ( ($duration / ( HRS )), ($duration % ( HRS )) );
+    (my $minutes, $duration) = ( ($duration / ( MIN )), ($duration % ( MIN )) );
+    return sprintf( '%02d:%02d:%02d', $hours, $minutes, $duration );
 }
 
 1;
@@ -237,20 +237,20 @@ MyPodcasts - Collects and builds Podcast information into an RSS stream
 
 =head1 SYNOPSIS
 
-	use MyPodcasts;
+    use MyPodcasts;
 
-	my @podcasts = MyPodcasts->list();
+    my @podcasts = MyPodcasts->list();
 
-	my $podcast = MyPodcasts->get_Config('foo');
+    my $podcast = MyPodcasts->get_Config('foo');
 
-	MyPodcasts->add_ID3_tag($podcast);
+    MyPodcasts->add_ID3_tag($podcast);
 
-	MyPodcasts->buildRSS( $podcast );
+    MyPodcasts->buildRSS( $podcast );
 
 =head1 DESCRIPTION
 
-	The main part of this module is it's config hash. It contains pieces of information to extract the
-	description information from a radio show's website and build a valid iTunes podcast feed. The value
-	of the 'extract' key must be an anonymous subroutine. All other values should be scalars.
+    The main part of this module is it's config hash. It contains pieces of information to extract the
+    description information from a radio show's website and build a valid iTunes podcast feed. The value
+    of the 'extract' key must be an anonymous subroutine. All other values should be scalars.
 
 =cut
