7e05f5bd9f773a995d849dd72bd772404ed132a4
Ivan Heffner <iheffner@gmail.com>
2014-08-18 17:28:12 -0700

    make %month and %day available outside MyPodcasts.pm

--- lib/MyPodcasts.pm
+++ lib/MyPodcasts.pm
@@ -15,11 +15,11 @@ use constant 'MIN'  => 60          ; # seconds in an minute
 use constant 'HRS'  => 60 * 60     ; # seconds in an hour
 use constant 'DAYS' => 60 * 60 * 24; # seconds in a day
 
-my %month = ( 0  => undef,
+our %month = ( 0  => undef,
     1  => 'January', 2  => 'February', 3  => 'March', 4  => 'April', 5  => 'May', 6  => 'June',
     7  => 'July', 8  => 'August', 9  => 'September', 10 => 'October', 11 => 'November', 12 => 'December',
 );
-my %day = ( 0 => 'Sun', 1 => 'Mon', 2 => 'Tue', 3 => 'Wed', 4 => 'Thu', 5 => 'Fri', 6 => 'Sat' );
+our %day = ( 0 => 'Sun', 1 => 'Mon', 2 => 'Tue', 3 => 'Wed', 4 => 'Thu', 5 => 'Fri', 6 => 'Sat' );
 
 sub new {
     my($class, @args) = @_;
